57a09dc66b0ab492cf43678ee5161a414134aa48
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2021-09-01 13:23:28 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -1,106 +1,32 @@
-var mySwiper = new Swiper('#ongoingHack', {
-    // Optional parameters
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 30,
-    breakpoints: {
-        375: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        425: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        756: {
-            slidesPerView: 2,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-     
-    }
-});
+{
+    var data = 0;
 
+document.getElementById("incdec").innerText = data;
+  
+//creation of increment function
+function increment() {
+    data = data + 1;
+    document.getElementById("incdec").innerText = data;
+}
+//creation of decrement function
+function decrement() {
+    data = data - 1;
+    document.getElementById("incdec").innerText = data;
+}
+}
 
-var mySwiper = new Swiper('#ongoingHacks', {
-    // Optional parameters
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 30,
-    breakpoints: {
-        375: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-        425: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-        756: {
-            slidesPerView: 2,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        1440: {
-            slidesPerView: 7,
-            spaceBetween: 5,
-        },
-    }
-});
+{
+    var datadown = 0;
 
-
-var mySwiper = new Swiper('#Testimonials', {
-    // Optional parameters
-    autoplay: {
-        delay: 5500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 30,
-    breakpoints: {
-        375: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-    }
-});
-
-
-var swiper = new Swiper('#initiative', {
-    // Optional parameters
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+document.getElementById("incdecdown").innerText = datadown;
+  
+//creation of increment function
+function incrementdown() {
+    datadown = datadown + 1;
+    document.getElementById("incdecdown").innerText = datadown;
+}
+//creation of decrement function
+function decrementdown() {
+    datadown = datadown - 1;
+    document.getElementById("incdecdown").innerText = datadown;
+}}
