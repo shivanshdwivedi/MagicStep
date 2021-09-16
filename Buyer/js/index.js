ce36755f53d1f0f04a702f55018e310d35ef87f8
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2021-09-16 20:25:29 +0530

    commit

--- Buyer/js/index.js
+++ Buyer/js/index.js
@@ -1,32 +1,43 @@
-{
-    var data = 0;
+// {
+//     var data = 0;
 
-document.getElementById("incdec").innerText = data;
+// document.getElementById("incdec").innerText = data;
   
-//creation of increment function
-function increment() {
-    data = data + 1;
-    document.getElementById("incdec").innerText = data;
-}
-//creation of decrement function
-function decrement() {
-    data = data - 1;
-    document.getElementById("incdec").innerText = data;
-}
-}
+// //creation of increment function
+// function increment() {
+//     data = data + 1;
+//     document.getElementById("incdec").innerText = data;
+// }
+// //creation of decrement function
+// function decrement() {
+//     data = data - 1;
+//     document.getElementById("incdec").innerText = data;
+// }
+// }
 
-{
-    var datadown = 0;
+// {
+//     var datadown = 0;
 
-document.getElementById("incdecdown").innerText = datadown;
+// document.getElementById("incdecdown").innerText = datadown;
   
-//creation of increment function
-function incrementdown() {
-    datadown = datadown + 1;
-    document.getElementById("incdecdown").innerText = datadown;
-}
-//creation of decrement function
-function decrementdown() {
-    datadown = datadown - 1;
-    document.getElementById("incdecdown").innerText = datadown;
-}}
+// //creation of increment function
+// function incrementdown() {
+//     datadown = datadown + 1;
+//     document.getElementById("incdecdown").innerText = datadown;
+// }
+// //creation of decrement function
+// function decrementdown() {
+//     datadown = datadown - 1;
+//     document.getElementById("incdecdown").innerText = datadown;
+// }}
+
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
